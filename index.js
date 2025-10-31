32a4977748e6883576286f34d0aea4ce0f2ce6f6
User <qazijawad456@gmail.com>
2025-10-31 23:24:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. Greet website visitor
+alert("Welcome to my website!");
+
+// 2. Display error message
+alert("Error! Please enter a valid password.");
+
+// 3. Display message with line break
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// 4. Display messages in sequence
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// we cannot use chekbox in alert message
+
+// 5. Log message to console
+  console.log("Hello... I can run JS through my web browser's console");
+
+// 6 and 7. The code for these tasks is included in the HTML file as script tags in different locations.
